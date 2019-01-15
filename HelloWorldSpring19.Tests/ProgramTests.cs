3dcc8670a5f9a089596503173dc962b6ad633e20
Jen E <jen@wecancodeit.org>
2019-01-15 11:28:15 -0500

    Initial commit

---
+++ HelloWorldSpring19.Tests/ProgramTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HelloWorldSpring19.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Hello_World_Test()
+        {
+            Assert.Equal("Hello", "Hello");
+        }
+    }
+}
---
+++ HelloWorldSpring19/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldSpring19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //This is a comment
+            //if I type something else
+        }
+    }
+}
